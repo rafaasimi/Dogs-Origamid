816b8a8ba9a761218a442977864f2689a75f5400
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-23 13:56:00 -0300

    Ajutes para input dinamico.

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
-      <label htmlFor={name} className={styles.label}>{label}</label>
-      <input id={name} name={name} className={styles.input} type={type}/>
-      <p className={styles.error}>Error</p>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
