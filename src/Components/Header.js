93790a2e9ef38438b6c5fd9a3d601c65d127c664
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-23 11:22:05 -0300

    Add classes e estilização do header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
-  return <div className={styles.header}>
-    <nav className="container">
-      <Link to="/" end>Home</Link>
-      <Link to="login" end>Login / Criar</Link>
-    </nav>
-  </div>;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
