17514a4000fd9466751773baedaff64b32661afb
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-23 10:46:50 -0300

    Import dos componentes e css reset.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
-      app
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
