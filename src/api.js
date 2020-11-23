18c63a21f14295ba2f754ef99147347eba729ab5
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-23 14:48:39 -0300

    Criação das funções para requests aos endpoints.

---
+++ src/api.js
@@ -0,0 +1,26 @@
+export const API_URL = "https://dogsapi.origamid.dev/json";
+
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + "/jwt-auth/v1/token",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function USER_GET(token) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
