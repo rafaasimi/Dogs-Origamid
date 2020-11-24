6a63fd7a315a4bc79ebe93fb05a111c1d25aa5b7
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-24 09:41:24 -0300

    Estilização da página de cadastro/login

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import useForm from "../../Hooks/useForm";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
 import { UserContext } from "../../UserContext";
+import Error from "../Helper/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -18,22 +21,34 @@ const LoginForm = () => {
     // Faz a verificação dos campos de username e password
     // Se TRUE, faz a solicitação a API em UserContext.js para obter o token do usuário.
     if (username.validate() && password.validate()) {
-        userLogin(username.value, password.value)
+      userLogin(username.value, password.value);
+    }
   }
-}
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        {loading ? <Button disabled>Carregando...</Button> : <Button>Entrar</Button>}
-        
-        {error && <p>{error}</p>}
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <Error error={error} />}
       </form>
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
 
-      <Link to="/login/criar">Cadastro</Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
