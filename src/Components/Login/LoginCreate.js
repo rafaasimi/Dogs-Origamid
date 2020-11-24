f3017627b9232cd1afc1ae448f0db455247a95f2
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-24 10:36:11 -0300

    Criado tela de cadastro e estilizado.

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginCreate = () => {
-    return (
-        <div>
-            Login Criar
-        </div>
-    )
-}
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+  const { userLogin } = React.useContext(UserContext);
 
-export default LoginCreate
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="E-mail" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginCreate;
