025d1c2086c04ac374cec24c59a030b46d890cf5
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-24 10:36:26 -0300

    Criado função de cadastro na API.

--- src/api.js
+++ src/api.js
@@ -39,3 +39,18 @@ export function USER_GET(token) {
     },
   };
 }
+
+
+// Cadastrar um usuario
+export function USER_POST(body) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
