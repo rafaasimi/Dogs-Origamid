04f3b61cbeb0615dc93e142c439c6a3632da5c54
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-25 11:40:28 -0300

    Criação de regra para validar numeros do formulario.

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um e-mail válido.",
   },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize somente números."
+  }
 };
 
 const useForm = (type) => {
