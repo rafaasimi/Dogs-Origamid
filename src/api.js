2688c7270b53bea5da852e10f1f88dbe7b7c1ebc
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-25 11:40:54 -0300

    Add funções para postar fotos e pegar fotos na API.

--- src/api.js
+++ src/api.js
@@ -54,3 +54,39 @@ export function USER_POST(body) {
     },
   };
 }
+
+// Envia as fotos do formulario para a API
+export function PHOTO_POST(formData, token) {
+  return {
+    url: API_URL + "/api/photo",
+    options: {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      body: formData,
+    },
+  };
+}
+
+// Solicita as fotos da API de acordo com os parametros passados
+export function PHOTOS_GET({page, total, user}) {
+  return {
+    url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
+    options: {
+      method: "GET",
+      cache: 'no-store'
+    },
+  };
+}
+
+// Solicita a foto da API de acordo com o ID
+export function PHOTO_GET(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "GET",
+      cache: 'no-store'
+    },
+  };
+}
