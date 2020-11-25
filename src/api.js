cf40866ecaf1956e9d9ac0d8478ea652939b6a4e
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-25 14:33:28 -0300

    API: Criação da função de deletar fotos.

--- src/api.js
+++ src/api.js
@@ -104,3 +104,16 @@ export function COMMENT_POST(id, body, token) {
     },
   };
 }
+
+// Deleta uma foto de acordo com o ID
+export function PHOTO_DELETE(id, token) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
