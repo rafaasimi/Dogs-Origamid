80970a10d7bd9f2660b9da51226b819eb5557d8c
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-25 13:56:19 -0300

    Ajustes estilos gerais.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return <div className={styles.footer}></div>;
 };
 
 export default Footer;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Feed from './Feed/Feed'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <section className="container mainContainer">
+            <Feed />
+        </section>
     )
 }
 
