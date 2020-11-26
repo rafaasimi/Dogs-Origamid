ae3700c0f26339a523249f7c8b5571ccb67959f8
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-26 11:29:03 -0300

    Add PropTypes no Feed para envio do usuario.

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import PropTypes from 'prop-types'
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -51,5 +52,12 @@ const Feed = ({ user }) => {
     </div>
   );
 };
+
+Feed.defaultProps = {
+  user: 0,
+}
+Feed.propTypes = {
+  user: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired])
+}
 
 export default Feed;
