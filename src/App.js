9aace75d642424959f5e1e50101fa1a07e26c08b
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-27 09:35:43 -0300

    Ajustes na renderização do main.

--- src/App.js
+++ src/App.js
@@ -14,10 +14,11 @@ import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
+          <main className="AppBody">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
@@ -26,6 +27,7 @@ function App() {
             <Route path="perfil/:user" element={<UserProfile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
