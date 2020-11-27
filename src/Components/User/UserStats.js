8162c08783641e58343ef8beccc0a0fa40b38861
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-27 09:36:46 -0300

    Criação da pagina de estatisticas.

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-    return (
-        <div>
-            Estatisticas
-        </div>
-    )
-}
+  const { data, error, loading, request } = useFetch();
 
-export default UserStats
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
+};
+
+export default UserStats;
