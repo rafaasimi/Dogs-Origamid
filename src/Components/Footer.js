563e1372a49f8a23cb95bb95b82ce68549fb2e47
Rafael Zorzenon <rafaasimi@hotmail.com>
2020-11-27 09:35:19 -0300

    Estilização do Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import {ReactComponent as Dogs} from '../Assets/dogs-footer.svg'
 
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return <footer className={styles.footer}>
+    <Dogs/>
+    <p>Dogs. Alguns direitos reservados. &copy;</p>
+  </footer>;
 };
 
 export default Footer;
